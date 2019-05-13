e21fc63659fec7ff98857e9d29c7c7a41132d395
Andrea Peretti <and.xet@gmail.com>
2019-05-13 19:57:37 +0200

    Made tile selectable

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    private static Tile selected;
+    private SpriteRenderer Renderer;
+
+    private void Start()
+    {
+        Renderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void Select()
+    {
+        Renderer.color = Color.grey;
+    }
+
+    public void Unselect()
+    {
+        Renderer.color = Color.white;
+    }
+
+    private void OnMouseDown()
+    {
+        if(selected != null)
+        {
+            selected.Unselect();
+        }
+        selected = this;
+        Select();
+    }
+}

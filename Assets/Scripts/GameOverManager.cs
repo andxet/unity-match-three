ee1ff724a78d7b8d381d1430141f46b4a9bb7bc0
Andrea Peretti <and.xet@gmail.com>
2019-05-18 00:57:08 +0200

    Added game menu and UI

---
+++ Assets/Scripts/GameOverManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverManager : MonoBehaviour
+{
+    public GameObject HighScoreAlert;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI highScoreText;
+
+    private void OnEnable()
+    {
+        int score = PlayerPrefs.GetInt("score");
+        int highScore;
+        if (PlayerPrefs.HasKey("highScore"))
+        {
+            highScore = PlayerPrefs.GetInt("highScore");
+        }
+        else 
+        {
+            highScore = 0; 
+        }
+        if (score > highScore)
+        {
+            PlayerPrefs.SetInt("highScore", score);
+            highScoreText.text = score.ToString();
+            HighScoreAlert.SetActive(true);
+        }
+        else
+        {
+            HighScoreAlert.SetActive(false);
+            highScoreText.text = highScore.ToString();
+        }
+        scoreText.text = score.ToString();
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+#if UNITY_EDITOR
+    [MenuItem("Match three/Reset high score")]
+    static void ResetHighScore()
+    {
+        PlayerPrefs.SetInt("highScore", 0);
+        PlayerPrefs.SetInt("score", 0);
+    }
+#endif
+}
